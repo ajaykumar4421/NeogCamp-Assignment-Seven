65fad7731064b3d49419e63287c2b9cd401e3499
ajaykumar4421 <ajay.kumar4421@gmail.com>
2021-08-23 15:33:22 +0530

    fix - klingon output

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+var txtInput = document.querySelector("#textarea");
+var btntranslate = document.querySelector("#click-translate");
+var btnOutput = document.querySelector("#btn-output");
+
+
+btntranslate.addEventListener("click",function getServerUrl(){
+    serverUrl = "https://api.funtranslations.com/translate/klingon.json";
+    var inputTranslate = serverUrl +"?"+"text="+txtInput.value;
+    fetch(inputTranslate)
+    .then(response => response.json())
+    .then( out => {btnOutput.innerText = out.contents.translated})
+});
